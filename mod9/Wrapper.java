c19d8145c8b3a5b2803f869165274845f84a9869
Nivaldo Ferreira de Souza Filho <nivaldopvp@gmail.com>
2023-02-01 18:09:25 -0300

    add file Wrapper

---
+++ mod9/Wrapper.java
@@ -0,0 +1,22 @@
+package mod9;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        exemploWrapper();
+        exemplo2();
+    }
+    private static void exemploWrapper() {
+        int num1 = 10;
+        Short num2 = (short) num1;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+
+    // Teste tipos complexos: Wrapper, variáveis primitivas e Autoboxing.
+    private static void exemplo2() {
+        Integer num1 = Integer.valueOf(10);
+        short num2 = 5;
+        Boolean num3 = num1 > num2;
+        System.out.println(num3);
+    }
+}
